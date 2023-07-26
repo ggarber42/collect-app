1901759e38a6ef36115e9a1b06f181210f672071
ggarber42 <ggarber@protonmail.com>
2023-07-25 23:07:32 -0300

    feat[wip]: add integration test skeleton

---
+++ integration_test/create_form_model_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:collect_app/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.widgetWithText(FloatingActionButton, 'Modelo'), findsOneWidget);
+    expect(find.text('Collectapp'), findsNothing);
+
+  });
+}
